9ad869680222d554393266fcc9fb4f89d0776a38
kaua-drar <kauadrar@gmail.com>
2024-07-11 01:12:54 -0300

    feat: create Lobby screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { colors } from "../styles";
+import { ButtonPair, Text } from "../components/shared";
+import { LoovieLogo } from "../components/svgs";
 
 export default function Lobby() {
   return (
-    <View>
-      <Text>Lobby</Text>
+    <View style={styles.container}>
+      <LoovieLogo />
+      <Text style={styles.title}>Seja bem vindo(a) ao Loovie</Text>
+      <ButtonPair leftLabel="Cadastrar" rightLabel="Entrar" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 48,
+    gap: 48,
+  },
+  title: {
+    color: colors.white,
+    fontSize: 24,
+  }
+})
