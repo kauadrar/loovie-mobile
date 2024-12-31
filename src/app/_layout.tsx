6ed9191956163963b6862c3d682d35e6954df3ba
Kauã Drar <78101033+kauadrar@users.noreply.github.com>
2024-12-30 23:37:13 -0300

    fix: white background (#4)
    * fix: white background under keyboard
    
    * fix: white background on screen transitions

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,11 @@
 import { AuthProvider } from '@/contexts';
 import { colors } from '@/styles';
+import { DarkTheme, ThemeProvider } from '@react-navigation/native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'expo-dev-client';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import * as SystemUI from 'expo-system-ui';
 import { StyleSheet } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { KeyboardProvider } from 'react-native-keyboard-controller';
@@ -21,6 +23,8 @@ const queryClient = new QueryClient({
   },
 });
 
+SystemUI.setBackgroundColorAsync(colors.background);
+
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
@@ -28,13 +32,23 @@ export default function RootLayout() {
         <SafeAreaProvider>
           <GestureHandlerRootView style={styles.container}>
             <KeyboardProvider statusBarTranslucent navigationBarTranslucent>
-              <StatusBar style="light" />
-              <Stack
-                screenOptions={{
-                  headerShown: false,
-                  navigationBarColor: colors.background,
+              <ThemeProvider
+                value={{
+                  ...DarkTheme,
+                  colors: {
+                    ...DarkTheme.colors,
+                    background: colors.background,
+                  },
                 }}
-              />
+              >
+                <StatusBar style="light" />
+                <Stack
+                  screenOptions={{
+                    headerShown: false,
+                    navigationBarColor: colors.background,
+                  }}
+                />
+              </ThemeProvider>
             </KeyboardProvider>
           </GestureHandlerRootView>
         </SafeAreaProvider>
