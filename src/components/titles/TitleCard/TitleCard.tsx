7147de36d589a769fd95c2e92d6de1e1734721df
kaua-drar <kauadrar@gmail.com>
2025-02-12 23:04:25 -0300

    fix: TitleCard image's dimensions

--- src/components/titles/TitleCard/TitleCard.tsx
+++ src/components/titles/TitleCard/TitleCard.tsx
@@ -1,25 +1,28 @@
-import { Text } from '@/components/shared';
 import { TmdbImageUrl } from '@/constants';
-import FastImage from '@d11/react-native-fast-image';
-import { Dimensions, TouchableOpacity } from 'react-native';
+import { Image } from 'expo-image';
+
+import { Text } from '@/components/shared';
+import { cssInterop } from 'nativewind';
+import { TouchableOpacity, View } from 'react-native';
 import { TitleCardProps } from './TitleCard.types';
 
-const { width: WIDTH } = Dimensions.get('window');
+const StyledImage = cssInterop(Image, {
+  className: 'style',
+});
 
 export function TitleCard({ name, posterPath }: TitleCardProps) {
-  const width = Math.floor((WIDTH - 64) / 3);
-  const height = width * 1.5;
-
   return (
-    <TouchableOpacity className={`items-center w-[${width}px] mx-2 gap-2`}>
-      <FastImage
-        source={{ uri: `${TmdbImageUrl.W342}${posterPath}` }}
-        className={`h-[${height}px] w-full rounded-lg`}
-        resizeMode="cover"
-      />
-      <Text className="w-full text-sm align-center" numberOfLines={2}>
+    <View className="items-center w-1/3 px-2 gap-2">
+      <TouchableOpacity className="flex-row">
+        <StyledImage
+          contentFit="cover"
+          source={{ uri: `${TmdbImageUrl.W342}${posterPath}` }}
+          className="flex-1 h-auto aspect-[2/3] rounded-lg"
+        />
+      </TouchableOpacity>
+      <Text className="w-full text-sm text-center" numberOfLines={2}>
         {name}
       </Text>
-    </TouchableOpacity>
+    </View>
   );
 }
