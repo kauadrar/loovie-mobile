a88efd485810f2cfecabb0d559d14ca9bdc5ae9f
kaua-drar <kauadrar@gmail.com>
2025-04-18 17:33:05 -0300

    feat: add action buttons to NewPostModal

--- src/components/post/NewPostModal.tsx
+++ src/components/post/NewPostModal.tsx
@@ -1,6 +1,8 @@
+import { colors } from '@/styles';
 import { postSchema } from '@/validators';
 import { BottomSheetView } from '@gorhom/bottom-sheet';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { CalendarClock, Camera, Images } from 'lucide-react-native';
 import React, {
   forwardRef,
   useCallback,
@@ -9,10 +11,34 @@ import React, {
   useRef,
 } from 'react';
 import { Controller, useForm } from 'react-hook-form';
-import { Keyboard, StyleSheet } from 'react-native';
+import { Keyboard, Pressable, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { BottomSheetTextArea } from '../bottom-sheet';
-import { BottomSheetModal, GorhomBottomSheetModal } from '../shared';
+import { Gif } from '../icons';
+import {
+  BottomSheetModal,
+  GorhomBottomSheetModal,
+  TooltipButton,
+} from '../shared';
+
+const options = [
+  {
+    Icon: Images,
+    label: 'Choose from gallery',
+  },
+  {
+    Icon: Camera,
+    label: 'Take a photo',
+  },
+  {
+    Icon: Gif,
+    label: 'Search for a GIF',
+  },
+  {
+    Icon: CalendarClock,
+    label: 'Schedule your post',
+  },
+];
 
 export const NewPostModal = forwardRef<GorhomBottomSheetModal>(
   function NewPostModal(props, ref) {
@@ -39,33 +65,35 @@ export const NewPostModal = forwardRef<GorhomBottomSheetModal>(
     return (
       <BottomSheetModal
         ref={bottomSheetModalRef}
-        snapPoints={['50%']}
+        snapPoints={[360]}
         enableDynamicSizing={false}
       >
-        <BottomSheetView>
-          <SafeAreaView edges={['bottom']} style={styles.container}>
-            <Controller
-              control={control}
-              name="content"
-              render={({ field: { onChange, ...field } }) => (
-                <BottomSheetTextArea
-                  label="Post"
-                  placeholder="Teste"
-                  onChangeText={onChange}
-                  {...field}
-                />
-              )}
-            />
-          </SafeAreaView>
+        <BottomSheetView className="flex-1">
+          <Pressable onPress={() => Keyboard.dismiss()} className="flex-1">
+            <SafeAreaView edges={['bottom']} className="px-4 pt-4 pb-10 gap-5">
+              <Controller
+                control={control}
+                name="content"
+                render={({ field: { onChange, ...field } }) => (
+                  <BottomSheetTextArea
+                    label="Post"
+                    placeholder="Teste"
+                    onChangeText={onChange}
+                    {...field}
+                  />
+                )}
+              />
+              <View className="flex-row items-center gap-3 px-1">
+                {options.map(({ Icon, label }) => (
+                  <TooltipButton label={label} key={label}>
+                    <Icon color={colors.gray1} />
+                  </TooltipButton>
+                ))}
+              </View>
+            </SafeAreaView>
+          </Pressable>
         </BottomSheetView>
       </BottomSheetModal>
     );
   },
 );
-
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 16,
-    paddingVertical: 40,
-  },
-});
